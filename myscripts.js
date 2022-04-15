1fb8bf2d272083e87b2bc6d8a9ddeac34aa59a31
elleyeung-mendoza <eyeungmendoza@protonmail.com>
2022-04-14 22:27:47 -0400

    added input

--- myscripts.js
+++ myscripts.js
@@ -2,6 +2,8 @@ let result = '';
 let playerScore = 0;
 let computerScore = 0;
 
+let playerSelection = prompt("Type rock, paper, or scissor");
+
 function computerPlay() {
     let random = ['rock', 'paper', 'scissor']
      return random[Math.floor(Math.random() * 3)];
@@ -14,32 +16,35 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             result = 'You lose! Paper beats rock!'
+            computerScore++
         }
         else if (computerSelection === 'scissor') {
             result = 'You win! Rock beats scissor!'
+            playerScore++
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             result = 'You win! Paper beats rock!'
+            playerScore++
         }
         else if (computerSelection === 'scissor') {
             result = 'You lose! Scissor beats paper!'
+            computerScore++
         }
     }
     else if (playerSelection === 'scissor') {
         if (computerSelection === 'rock') {
             result = 'You lose! Rock beats scissor!'
+            computerScore++
         }
         else if (computerSelection === 'paper') {
             result = 'You win! Scissor beats paper!'
+            playerScore++
         }
     }
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+let computerSelection = computerPlay();
+playRound(playerSelection, computerSelection);
 console.log(result);
+
