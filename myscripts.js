e8e6d97e7c9986b0e49b7be5b39fd54ee86ccf87
elleyeung-mendoza <eyeungmendoza@protonmail.com>
2022-04-14 20:46:32 -0400

    add rock paper scissors

---
+++ myscripts.js
@@ -0,0 +1,43 @@
+function userPlay() {
+    let random = ["rock", "paper", "scissors"];
+return random[Math.floor(Math.random() * 3)];
+}
+function computerPlay() { //computer generates a random answer.
+let random = ["rock", "paper", "scissors"];
+return random[Math.floor(Math.random() * 3)];
+
+}
+function playRound(playerSelection, computerSelection) { //plays a round of the game.
+if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+        return "Draw!";
+    } else if (computerSelection === "paper") {
+        return "Computer wins!";
+    } else {
+        return "User wins!";
+    }
+} else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+        return "User wins!";
+    } else if (computerSelection === "paper") {
+        return "Draw!";
+    } else {
+        return "Computer wins!";
+    }
+} else {
+    if (computerSelection === "rock") {
+        return "Computer wins!";
+    } else if (computerSelection === "paper") {
+        return "User wins!";
+    } else {
+        return "Draw!";
+    }
+}
+}
+
+var userChoice = userPlay();
+var computerSelection = computerPlay();
+var result = playRound(userChoice, computerSelection)
+console.log("user's choice", userChoice);
+console.log("computer's choice", computerSelection);
+console.log("Result is", result);
